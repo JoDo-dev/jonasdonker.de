8a784f96471e5fa0e21029c408e99b628cd9b911
JoDo-dev <54305854+JoDo-dev@users.noreply.github.com>
2020-09-30 15:54:51 +0200

    changed deployment

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,16 @@
 
 # abort on errors
 set -e
-
+npm ci
 npm run build
 
 git checkout release
 git merge master
 
+rm /*
+rmdir -r public src
+mv -v /dist/* /
 # navigate into the build output directory
-cd dist
 git add -A
 git commit -m 'deploy'
 
