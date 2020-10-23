0532b04edab1f1ef1942aa17928c78adbe3473cc
JoDo-dev <54305854+JoDo-dev@users.noreply.github.com>
2020-10-23 15:44:54 +0200

    updated lint errors and favicon

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,18 +1,18 @@
 export default [
-  {
-    text: 'Ain´t no Mountain high enough.',
-    author: 'Marvin Gaye',
-  },
-  {
-    text: 'We are the Champions my friend.',
-    author: 'Queen',
-  },
-  {
-    text: 'Hello, is it me you`re looking for?',
-    author: 'Lionel Richie',
-  },
-  {
-    text: 'What you want baby, I got it.',
-    author: 'Aretha Franklin',
-  },
+    {
+        text: 'Ain´t no Mountain high enough.',
+        author: 'Marvin Gaye',
+    },
+    {
+        text: 'We are the Champions my friend.',
+        author: 'Queen',
+    },
+    {
+        text: 'Hello, is it me you`re looking for?',
+        author: 'Lionel Richie',
+    },
+    {
+        text: 'What you want baby, I got it.',
+        author: 'Aretha Franklin',
+    },
 ];
